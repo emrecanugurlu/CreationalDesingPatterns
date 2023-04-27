ef88dc6d64a92a68e772bac7385dbda048c42096
Emre Can <emrecnugurlu@gmail.com>
2023-04-27 14:25:35 +0300

    Multiton Design Patttern uygulamalı örnek.

---
+++ MultitonDesignPattern/MultitonDesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿//Uygulama bazında bir sınıfın sınırlı sayıda intance’sinin var olmasını sağlayan stratejidir.
+var i1 = Example.GetInstance("mySql");
+var i2 = Example.GetInstance("mongoDb");
+var i3 = Example.GetInstance("MSSQL");
+
+class Example
+{
+    private Example() {
+        Console.WriteLine($"{nameof(Example)} nesnesi oluşturuldu...");
+    }
+
+    static Dictionary<string, Example> instanceDictionary;
+
+    public static Example GetInstance(string key)
+    {
+        if (!instanceDictionary.ContainsKey(key))
+        {
+            instanceDictionary[key] = new Example();
+        }
+        return instanceDictionary[key];
+    }
+}
